dc12b53b16e6f263dea9a51c4347bca8b3d85ab9
M Srivardhan <srivardhanmanche@gmail.com>
2026-01-01 18:06:19 +0530

    users

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { useAuthenticator } from '@aws-amplify/ui-react';
 const client = generateClient<Schema>();
 
 function App() {
+  const { user, signOut } = useAuthenticator();
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
 
@@ -25,11 +26,11 @@ function App() {
     client.models.Todo.delete({ id })
   }
 
-  const { signOut } = useAuthenticator();
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
+
       <button onClick={createTodo}>+ new</button>
 
       <ul>
